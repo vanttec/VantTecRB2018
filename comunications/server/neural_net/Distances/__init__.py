814e6079e81bdda31d813f67dcaad444912f9186
carlosaraujoitesm <carlos.perez06@iest.edu.mx>
2018-05-17 18:46:39 -0500

    test

--- comunications/server/neural_net/Distances/__init__.py
+++ comunications/server/neural_net/Distances/__init__.py
@@ -1,1 +1,4 @@
 from .path import receive
+
+
+
--- comunications/server/neural_net/Distances/path.py
+++ comunications/server/neural_net/Distances/path.py
@@ -1,3 +1,10 @@
 def receive(values):
     if len(values) == 5:
         return True
+
+
+def get_distances(rois):
+	print '[%s]' % ', '.join(map(str, rois))
+	return True
+
+
