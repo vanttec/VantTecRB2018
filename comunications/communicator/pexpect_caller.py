830293787087ede0e083cf9d3a7118eb3766453f
julio177 <julioc.aguilarv@gmail.com>
2018-05-20 15:34:40 -0600

    Funcion generate data crea decimales con 2 de precision, parse data elimina lambda

--- comunications/communicator/pexpect_caller.py
+++ comunications/communicator/pexpect_caller.py
@@ -2,18 +2,19 @@ import time
 import pexpect
 from pexpect import popen_spawn
 from random import randint
-from Distances import get_distances
+from distances import get_distances
 
 TIME_DIVIDER = 10.0
 MAX_TIME = 30
 
 def generate_data():
+    '''Funcion para generar coordenadas aleatorias de objetos'''
     result = []
     for i in range(10):
-        x1 = randint(1, 50)
-        y1 = randint(50, 100)
-        x2 = randint(50, 100)
-        y2 = randint(1, 50)
+        x1 = round(randint(1, 50) * 1.0 + randint(0, 100) / 100.00, 2)
+        y1 = round(randint(50, 100) * 1.0 + randint(0, 100) / 100.00, 2)
+        x2 = round(randint(50, 100) * 1.0 + randint(0, 100) / 100.00, 2)
+        y2 = round(randint(1, 50) * 1.0 + randint(0, 100) / 100.00, 2)
         result.append([x1, y1, x2, y2])
     return result
 
@@ -24,7 +25,7 @@ def parse_data(data):
     values = data[(start_index + 1):-2]
     coordinates = values.split(",")
     # Parse to float the coordinate values
-    float_values = list(map(lambda x: float(x), coordinates))
+    float_values = list(map(float, coordinates))
     return float_values
 
 
@@ -38,14 +39,14 @@ def call():
     # while counter < 31:
     #     child.expect('.*')
     #     counter += 1
-    # Expected output from darknet: {b, 0.583, (2.5, 30.58, 78.5, 4.78)}
+    # Expected output from darknet: "{b, 0.583, (2.5, 30.58, 78.5, 4.78)}"
     start_time = time.time() # Use this for simulation of time
     while (time.time() - start_time) < MAX_TIME:
         # Expect outputs
         # child.expect('.*')
         # Print for debugging
         # print(child.after.decode("utf-8"), end='')
-        # TODO Parse data of child.after
+        # Parse data string of child.after
         # data = parse_data(child.after.decode("utf-8"))
         # NOTE x1 < x2, y1 > y2
         # random sleep time for testing
