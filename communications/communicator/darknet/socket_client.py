cf59cd2b6dc83aa0086d26b237f8d1583a956845
julio177 <julioc.aguilarv@gmail.com>
2018-06-19 13:56:28 -0600

    Archivos para comunicacion con dron y rx

---
+++ communications/communicator/darknet/socket_client.py
@@ -0,0 +1,32 @@
+'''
+Test module for connection with sockets
+'''
+import socket
+
+def main():
+    '''Main function to test connection'''
+    host = '127.0.0.1'
+    port = 5000
+
+    my_socket = socket.socket()
+    my_socket.connect((host, port))
+
+    # messages = ['DockNumkthanksbye3421',
+    #             'DockNumkthanksbye21',
+    #             'DockNumkthanksbye1',
+    #             'DockNumkthanksbye3',
+    #             'DockNumkthanksbye2',
+    #             'Mapkthanksbye89205,205020.59303,4930',
+    #             'Mapkthanksbye85.935,50.593,4930,ghtiw',
+    #             'DockPhoto,500,480,10kthanksbye57khgs23iurih']
+
+    message = input('-> ')
+    while message != 'q':
+        my_socket.send(message.encode())
+        data = my_socket.recv(1024).decode()
+        print('Received from server: ' + data)
+        message = input('-> ')
+
+    my_socket.close()
+
+main()
