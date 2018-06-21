90715404345aff9a9a3ccb323c962850f7ede12f
julio177 <julioc.aguilarv@gmail.com>
2018-06-20 21:30:44 -0500

    Crea clase para boat data

--- mission_data.py
+++ mission_data.py
@@ -2,9 +2,25 @@
 Module with class that holds all variables and data for the mission
 '''
 
+class MissionStationData:
+    '''Class to handle global varaibles easily'''
+    def __init__(self, station_xb):
+        self.dock_num = 0
+        self.map_data = {
+            'height': 0,
+            'width': 0,
+            'obstacles': [], #tuple array
+            'boat_x': 0,
+            'boat_y': 0,
+            'can_x': 0,
+            'can_y': 0,
+        },
+        self.received_dock_img = False
+        self.station_xb = station_xb
+
 class MissionBoatData:
     '''Class to handle global varaibles easily'''
-    def __init__(self, boat_xb, station_xb):
+    def __init__(self, boat_xb):
         self.dock_num = 0
         self.map_data = {
             'height': 0,
