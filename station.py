7b36fede09f6837c061503d74c7fe2d9e1175b75
Javier Meza <javenmeza@gmail.com>
2018-06-20 23:00:25 -0400

    XB Test

--- station.py
+++ station.py
@@ -4,16 +4,16 @@ from mission_data import MissionStationData
 
 def main_station():
     main_data = MissionStationData(xbee())
-    thread_drone = DroneThread(1, "DroneComm", main_data)
-    thread_check = DroneCheker(2, "Drone Check", main_data.dock_num, main_data.map_data)
+    # thread_drone = DroneThread(1, "DroneComm", main_data)
+    # thread_check = DroneCheker(2, "Drone Check", main_data.dock_num, main_data.map_data)
     thread_station = StationXbThread(3, "Station", main_data.station_xb)
 
-    thread_drone.start()
-    thread_check.start()
+    #thread_drone.start()
+    #thread_check.start()
     thread_station.start()
 
-    thread_drone.join()
-    thread_check.join()
+    #thread_drone.join()
+    #thread_check.join()
     thread_station.join()
 
 
