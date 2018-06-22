09c1cad65e34a832cd9a605f1d7f0ef5d4d2fae5
gerardoberni <gerardoberni19@gmail.com>
2018-06-22 12:38:54 -0400

    agregar codigo para poder pedir solo coordenadas

--- communications/Xbee/publisher.py
+++ communications/Xbee/publisher.py
@@ -25,11 +25,19 @@ def publisher(xbee):
         if remote_device is None:
             print("Could not find the remote device")
             exit(1)
-
-        print("Dame la latitud y longitud del waypoint donde se iniciara el challenge: ")
-        lat = input("\n lat: ")
-        lon = input("\n lon: ")
-        xbee.set_target(lat,lon)
+        print(" +--------------------------------------+")
+        print(" |    Give 1 to receive GPS coords      |")
+        print(" |    Give 2 to send target coords     |")
+        print(" +--------------------------------------+\n")
+        action = input("action: ")
+        xbee.set_action(action)
+        if action == '1':
+            xbee.set_target('0.000','0.000')
+        else:
+            print("Dame la latitud y longitud del waypoint donde se iniciara el challenge: ")
+            lat = input("\n lat: ")
+            lon = input("\n lon: ")
+            xbee.set_target(lat,lon)
         device.send_data(remote_device, xbee.send())
         while True:
             xbee_message = device.read_data()
@@ -39,9 +47,15 @@ def publisher(xbee):
                 print ("\n")
                 xbee_network = device.get_network()
                 remote_device = xbee_network.discover_device(REMOTE_NODE_ID)
-                lat = input("\n lat: ")
-                lon = input("\n lon: ")
-                xbee.set_target(lat,lon)
+                action = input("action: ")
+                xbee.set_action(action)
+                if action == '1':
+                    xbee.set_target('0.000','0.000')
+                else:
+                    print("Dame la latitud y longitud del waypoint destino: ")
+                    lat = input("\n lat: ")
+                    lon = input("\n lon: ")
+                    xbee.set_target(lat,lon)
                 device.send_data(remote_device, xbee.send())
 
         print("Success")
