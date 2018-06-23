db4c8845886349cf1d580993b17797111c88d9ff
gerardoberni <gerardoberni19@gmail.com>
2018-06-23 17:16:37 -0400

    instanciar motors

--- communications/boat_nav/navigation.py
+++ communications/boat_nav/navigation.py
@@ -28,7 +28,7 @@ class Navigation:
         self.frame = None
         self.stopNavigation = False
         self.imu = imu
-        
+        self.motors = Motors()
 
     def navigate(self, destiny, lat, lon):
         lastOrientationDegree = 0
@@ -36,7 +36,7 @@ class Navigation:
         turn_degrees_accum    = 0
         distance = destiny['distance']
         orientationDegree = destiny['degree']
-        motors = Motors()
+        
         #clean angle
         self.imu.get_delta_theta()
         #print("delta theta: ", self.imu.get_delta_theta)
@@ -78,21 +78,21 @@ class Navigation:
             if math.fabs(turn_degrees_needed) < 15: 
                 print("Tengo un margen menor a 15 grados")
                 velocity = 30
-                motors.move(velocity, velocity)
+                self.motors.move(velocity, velocity)
 
             else:
                 #girar
                 if turn_degrees_needed > 0:
                     #print("Going to move left")
-                    motors.move(15, -15)
+                    self.motors.move(15, -15)
                 else: 
                     #print("Going to move right")
-                    motors.move(-15, 15)
+                    self.motors.move(-15, 15)
             #ir derecho
             #recorrer 2 metros
             destiny = self.imu.get_degrees_and_distance_to_gps_coords(lat, lon)
             #time.sleep(1)
-        motors.move(0,0)
+        self.motors.move(0,0)
 
     def visnavigate(self, pdistance, pdegree, status):
         lastOrientationDegree = 0
@@ -100,7 +100,6 @@ class Navigation:
         turn_degrees_accum    = 0
         distance = pdistance
         orientationDegree = pdegree
-        motors = Motors()
         #clean angle
         self.imu.get_delta_theta()
         #print("delta theta: ", self.imu.get_delta_theta)
@@ -142,16 +141,16 @@ class Navigation:
             if math.fabs(turn_degrees_needed) < 15: 
                 print("Tengo un margen menor a 15 grados")
                 velocity = 50
-                motors.move(velocity, velocity)
+                self.motors.move(velocity, velocity)
 
             else:
                 #girar
                 if turn_degrees_needed > 0:
                     #print("Going to move left")
-                    motors.move(15, -15)
+                    self.motors.move(15, -15)
                 else: 
                     #print("Going to move right")
-                    motors.move(-15, 15)
+                    self.motors.move(-15, 15)
             #ir derecho
             #recorrer 2 metros           
             #time.sleep(1)
@@ -166,9 +165,9 @@ class Navigation:
             destiny = self.imu.get_degrees_and_distance_to_gps_coords(lat, lon)
             self.navigate(destiny,lat,lon)
             
-        motors.move(50,50)
+        self.motors.move(50,50)
         time.sleep(5)
-        motors.move(0,0)
+        self.motors.move(0,0)
 
     def visnavigate2(self, pdistance, pdegree):
         lastOrientationDegree = 0
@@ -177,7 +176,6 @@ class Navigation:
         distance = pdistance
         orientationDegree = pdegree
         #clean angle
-        motors = Motors()
         self.imu.get_delta_theta()
         #print("delta theta: ", self.imu.get_delta_theta)
 
@@ -218,25 +216,24 @@ class Navigation:
             if math.fabs(turn_degrees_needed) < 15: 
                 print("Tengo un margen menor a 15 grados")
                 velocity = 50
-                motors.move(velocity, velocity)
+                self.motors.move(velocity, velocity)
 
             else:
                 #girar
                 if turn_degrees_needed > 0:
                     #print("Going to move left")
-                    motors.move(15, -15)
+                    self.motors.move(15, -15)
                 else: 
                     #print("Going to move right")
-                    motors.move(-15, 15)
+                    self.motors.move(-15, 15)
             #ir derecho
             #recorrer 2 metros           
             #time.sleep(1)       
 
-        motors.move(50,50)
+        self.motors.move(50,50)
         time.sleep(5)
-        motors.move(0,0)
+        self.motors.move(0,0)
 
     def search(self):
-        motors = Motors()
         print('Searching')
-        motors.move(30,-30)
+        self.motors.move(30,-30)
