76e0a7907fb2e6786f9df16fc4a5f812613ae6d2
gerardoberni <gerardoberni19@gmail.com>
2018-06-23 16:26:18 -0400

    otro test

--- communications/boat_nav/test_motors.py
+++ communications/boat_nav/test_motors.py
@@ -1,8 +1,14 @@
 from motors import Motors
-
+import serial
+import time
 def kill_me():
-    motors = Motors()
-    while True:
-        motors.move_thrusters(1600,1600)
-
+    ser = serial.Serial('/dev/ttyACM0', 115200)
+    val = '%B,1600,1600%'
+    val2 = '%B,1500,1500%'
+    val = val.encode()
+    val2 = val2.encode()
+    ser.write(val)
+    time.sleep(15)
+    ser.write(val2)
+    ser.close()
 kill_me()
